b6e708876250c2974927e7005c990acf30772de8
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-05-31 13:50:01 -0300

    install and generate types from codegen

---
+++ src/types/types.d.ts
@@ -0,0 +1,193 @@
+export type Maybe<T> = T | null;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  DateTime: Date;
+};
+
+
+export type Query = {
+  __typename?: 'Query';
+  me: Account;
+  getDoctor: Doctor;
+  getDoctors?: Maybe<Array<Doctor>>;
+  getGuide: Guide;
+  getGuides?: Maybe<Array<Guide>>;
+  getPatient: Patient;
+  getPatients?: Maybe<Array<Patient>>;
+};
+
+
+export type QueryGetDoctorArgs = {
+  id: Scalars['ID'];
+};
+
+
+export type QueryGetGuideArgs = {
+  id: Scalars['ID'];
+};
+
+
+export type QueryGetPatientArgs = {
+  id: Scalars['ID'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createAccount: Account;
+  signIn: LoginUser;
+  createDoctor: Doctor;
+  updateDoctor: Doctor;
+  createGuide: Guide;
+  updateGuides: Guide;
+  createPatient: Patient;
+  updatePatient: Patient;
+};
+
+
+export type MutationCreateAccountArgs = {
+  input: CreateAccountInput;
+};
+
+
+export type MutationSignInArgs = {
+  input: LoginUserInput;
+};
+
+
+export type MutationCreateDoctorArgs = {
+  input: DoctorInput;
+};
+
+
+export type MutationUpdateDoctorArgs = {
+  id: Scalars['ID'];
+  input: DoctorInput;
+};
+
+
+export type MutationCreateGuideArgs = {
+  input: GuideInput;
+};
+
+
+export type MutationUpdateGuidesArgs = {
+  id: Scalars['ID'];
+  input: GuideInput;
+};
+
+
+export type MutationCreatePatientArgs = {
+  input: PatientInput;
+};
+
+
+export type MutationUpdatePatientArgs = {
+  id: Scalars['ID'];
+  input: PatientInput;
+};
+
+export type Gender = 
+  | 'MASCULINO'
+  | 'FEMININO';
+
+export type Plan = 
+  | 'STARTER';
+
+export type CreateAccountInput = {
+  email: Scalars['String'];
+  password: Scalars['String'];
+  name: Scalars['String'];
+  plan?: Maybe<Plan>;
+};
+
+export type LoginUserInput = {
+  email: Scalars['String'];
+  password: Scalars['String'];
+};
+
+export type LoginUser = {
+  __typename?: 'LoginUser';
+  token: Scalars['String'];
+  user: User;
+};
+
+export type Account = {
+  __typename?: 'Account';
+  id: Scalars['ID'];
+  plan: Plan;
+  name?: Maybe<Scalars['String']>;
+  users?: Maybe<Array<Maybe<User>>>;
+  status?: Maybe<Scalars['Boolean']>;
+  createdAt?: Maybe<Scalars['DateTime']>;
+};
+
+export type User = {
+  __typename?: 'User';
+  id: Scalars['ID'];
+  accountId: Scalars['ID'];
+  status?: Maybe<Scalars['Boolean']>;
+  email?: Maybe<Scalars['String']>;
+  name?: Maybe<Scalars['String']>;
+  gender?: Maybe<Gender>;
+  roles?: Maybe<Array<Scalars['String']>>;
+  createdAt?: Maybe<Scalars['DateTime']>;
+  updatedAt?: Maybe<Scalars['DateTime']>;
+};
+
+export type DoctorInput = {
+  name: Scalars['String'];
+  gender?: Maybe<Gender>;
+  birth?: Maybe<Scalars['DateTime']>;
+  register?: Maybe<Scalars['String']>;
+};
+
+export type Doctor = {
+  __typename?: 'Doctor';
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  gender?: Maybe<Gender>;
+  birth?: Maybe<Scalars['DateTime']>;
+  register?: Maybe<Scalars['String']>;
+  createdAt?: Maybe<Scalars['DateTime']>;
+  updatedAt?: Maybe<Scalars['DateTime']>;
+  guides?: Maybe<Array<Maybe<Guide>>>;
+};
+
+export type GuideInput = {
+  description: Scalars['String'];
+  patientId: Scalars['ID'];
+  doctorId: Scalars['ID'];
+};
+
+export type Guide = {
+  __typename?: 'Guide';
+  id: Scalars['ID'];
+  description: Scalars['String'];
+  doctor?: Maybe<Doctor>;
+  patient?: Maybe<Patient>;
+  createdAt?: Maybe<Scalars['DateTime']>;
+  updatedAt?: Maybe<Scalars['DateTime']>;
+};
+
+export type Patient = {
+  __typename?: 'Patient';
+  guides?: Maybe<Array<Maybe<Guide>>>;
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  gender?: Maybe<Gender>;
+  birth?: Maybe<Scalars['DateTime']>;
+  createdAt?: Maybe<Scalars['DateTime']>;
+  updatedAt?: Maybe<Scalars['DateTime']>;
+};
+
+export type PatientInput = {
+  name: Scalars['String'];
+  birth?: Maybe<Scalars['DateTime']>;
+  gender?: Maybe<Gender>;
+};
+
