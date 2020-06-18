b7c09f3ca84f74b0d89234e16dabd7ee0d202113
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-06-18 20:43:12 -0300

    create getUser hook

---
+++ src/hooks/useGetUser/index.tsx
@@ -0,0 +1,36 @@
+import gql from 'graphql-tag';
+import { useLazyQuery } from '@apollo/react-hooks';
+
+
+interface IUseGetUser {
+  getUser(): void;
+  queryResults: any;
+}
+
+export const GET_USER = gql`
+  query GetUser{
+    getUser {
+      id
+      status
+      email
+      name
+      gender
+      roles {
+        id
+        role
+      }
+    }
+  }
+`;
+
+export const useGetUser = (): IUseGetUser => {
+  const [query, queryResults] = useLazyQuery(GET_USER, {
+    onCompleted: (data) => {
+      console.log(data);
+    },
+  });
+
+  const getUser = () => (query());
+
+  return { getUser, queryResults };
+};
