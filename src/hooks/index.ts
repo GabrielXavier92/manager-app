1ebf0fea22c7e16088bc360baa4112a2fe9f9152
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-06-19 09:37:56 -0300

    create default exports for hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,14 @@
+import useAuthToken from './useAuthToken';
+import useGetDoctors from './useGetDoctors';
+import useGetUser from './useGetUser';
+import useLogout from './useLogout';
+import useSignInMutation from './useSignInMutation';
+
+
+export {
+  useAuthToken,
+  useGetDoctors,
+  useGetUser,
+  useLogout,
+  useSignInMutation,
+};
