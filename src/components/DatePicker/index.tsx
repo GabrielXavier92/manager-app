cea9a1dc810be1eb02c482be6ec5dbc15bcd51a7
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-06-26 22:56:57 -0300

    create datePicker component

---
+++ src/components/DatePicker/index.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import {
+  TextField, Popover, DatePicker as DatePickerPolaris,
+} from '@shopify/polaris';
+
+import { Controller } from 'react-hook-form';
+import { formatDate } from '../../utils/formatDate';
+
+interface IDatePicker {
+  control: any;
+  setValue: any
+  name: string;
+  label?: string
+}
+
+const DatePicker: React.FC<IDatePicker> = ({
+  control, setValue, name, label = 'Data',
+}) => {
+  const [popoverActive, setPopoverActive] = useState(false);
+
+  const date = new Date();
+
+  const [{ month, year }, setDate] = useState({
+    month: date.getMonth(),
+    year: date.getFullYear(),
+  });
+
+
+  const [selectedDates, setSelectedDates] = useState({
+    start: new Date(),
+    end: new Date(),
+  });
+
+  const handleMonthChange = (newMonth: number, newYear: number) => setDate({ month: newMonth, year: newYear });
+
+
+  const togglePopoverActive = () => setPopoverActive(!popoverActive);
+
+  const activator = (
+    <div onClick={togglePopoverActive}>
+      <Controller
+        as={(
+          <TextField type="text" placeholder="Selecione uma data" label={label} onChange={() => { }} />
+      )}
+        control={control}
+        name={name}
+        defaultValue={selectedDates.start}
+      />
+    </div>
+  );
+
+  return (
+    <Popover
+      active={popoverActive}
+      activator={activator}
+      onClose={togglePopoverActive}
+      preferredAlignment="left"
+    >
+      <DatePickerPolaris
+        month={month}
+        year={year}
+        onChange={(dates) => {
+          const { start, end } = dates;
+          setValue(name, formatDate(start));
+          setSelectedDates({ start, end });
+        }}
+        onMonthChange={handleMonthChange}
+        selected={selectedDates}
+      />
+    </Popover>
+  );
+};
+
+export default DatePicker;
