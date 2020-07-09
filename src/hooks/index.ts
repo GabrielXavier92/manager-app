a1f23d0b4ed4813cd3db2a318ede9632f1357578
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-07-09 11:37:23 -0300

    add new roots to default exports

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,12 +1,15 @@
-
 import useAuthToken from './useAuthToken';
 import useDoctor from './useDoctor';
 import useSpecialty from './useSpecialty';
+import useProcedureTable from './useProcedureTable';
+import useProcedure from './useProcedure';
 import useUser from './useUser';
 
 export {
   useAuthToken,
   useDoctor,
   useSpecialty,
+  useProcedureTable,
+  useProcedure,
   useUser,
 };
