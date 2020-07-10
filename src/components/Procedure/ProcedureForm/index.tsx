0ff193628f1c5020cc5ab434f80634bf5cd9750a
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-07-10 17:44:31 -0300

    create procedure form

--- src/components/Procedure/ProcedureForm/index.tsx
+++ src/components/Procedure/ProcedureForm/index.tsx
@@ -1,58 +1,174 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 import {
-  Button, Card, TextField, Stack, InlineError,
+  Button, Card, TextField, Stack, Page, Form, Layout, FormLayout, Select,
 } from '@shopify/polaris';
+import { useForm, Controller } from 'react-hook-form';
 
-import { Controller } from 'react-hook-form';
+import { ProcedureInput, Specialty } from '../../../types/types.d';
+import {
+  useGetProcedureLazyQuery, useCreateProcedureMutation, useUpdateProcedureMutation, useGetSpecialtiesQuery,
+} from '../../../generated/graphql';
+import { GET_PROCEDURES, GET_PROCEDURE } from '../gql';
 
-import { DeleteMajorMonotone } from '@shopify/polaris-icons';
-
-interface IProcedureForm {
-  item: any;
-  index: number;
-  control: any;
-  remove: any;
-  error: boolean;
+interface RouteParams {
+  id: string;
+  procedureTableId: string;
 }
 
-const ProcedureForm: React.FC<IProcedureForm> = ({
-  item, index, control, remove, error,
-}) => (
-  <Card sectioned>
-    <Stack spacing="tight" distribution="fillEvenly" alignment="trailing">
-      <Controller
-        as={<TextField type="text" label="Nome do procedimento" onChange={() => { }} />}
-        control={control}
-        rules={{ required: true }}
-        name={`procedures[${index}].name`}
-        defaultValue={`${item.name}`}
-      />
-      <Controller
-        as={<TextField type="text" label="Codigo" onChange={() => { }} />}
-        control={control}
-        name={`procedures[${index}].code`}
-        defaultValue={`${item.code}`}
-      />
-      <Controller
-        as={<TextField type="text" label="Valor" onChange={() => { }} />}
-        control={control}
-        name={`procedures[${index}].value`}
-        defaultValue={`${item.value}`}
-      />
+interface ISelect {
+  value: string;
+  label: string;
+}
 
-      <div style={{ display: 'none' }}>
-        <Controller
-          as={<TextField type="text" label="ID" onChange={() => { }} />}
-          control={control}
-          name={`procedures[${index}].id`}
-          defaultValue={`${item.id}`}
-        />
-      </div>
+const ProcedureForm: React.FC = () => {
+  const history = useHistory();
+  const params = useParams<RouteParams>();
 
-      <Button destructive icon={DeleteMajorMonotone} onClick={() => { remove(index); }} />
-    </Stack>
-    <InlineError message={error ? 'Nome do procedimento obrigatorio' : ''} fieldID={`procedures[${index}].name`} />
-  </Card>
-);
+  const [title, setTitle] = useState('Editar Dados');
+  const [specialties, setSpecialties] = useState<Array<ISelect>>([]);
+
+  const {
+    control, errors, handleSubmit, reset,
+  } = useForm<ProcedureInput>();
+
+  const { data: fetchSpecialties } = useGetSpecialtiesQuery();
+  const [getProcedure, { data }] = useGetProcedureLazyQuery();
+  const [createProcedure, { loading: createLoading }] = useCreateProcedureMutation({
+    onCompleted: (newProcedure) => {
+      if (newProcedure) history.push(`/procedureTable/${params.procedureTableId}`);
+    },
+  });
+  const [updateProcedure, { loading: updateLoading }] = useUpdateProcedureMutation({
+    onCompleted: (newProcedure) => {
+      if (newProcedure) history.push(`/procedureTable/${params.procedureTableId}`);
+    },
+  });
+
+  const handleGetProcedure = () => {
+    if (params.id) {
+      getProcedure({ variables: { id: params.id } });
+    } else {
+      setTitle('Novo procedimento');
+    }
+  };
+
+  useEffect(handleGetProcedure, [params.id]);
+
+  const handleSetFormValues = () => {
+    if (data?.getProcedure) {
+      const procedure = {
+        ...data.getProcedure,
+        specialtyId: data?.getProcedure?.specialty!.id,
+      };
+      reset(procedure);
+    }
+  };
+
+  useEffect(handleSetFormValues, [data]);
+
+  const onSubmit = (input: ProcedureInput) => {
+    const newProcedure = {
+      ...input,
+      procedureTableId: params.procedureTableId,
+    };
+    if (params.id) {
+      updateProcedure({ variables: { id: params.id, input: newProcedure } });
+    } else {
+      createProcedure({ variables: { input: newProcedure } });
+    }
+  };
+
+  const handleSetOptions = () => {
+    if (fetchSpecialties?.getSpecialties) {
+      const selectSpecialties = fetchSpecialties?.getSpecialties!.map((specialty: Specialty) => ({
+        value: specialty.id,
+        label: specialty.name,
+      }));
+      setSpecialties(selectSpecialties);
+    }
+  };
+
+  useEffect(handleSetOptions, [fetchSpecialties?.getSpecialties]);
+
+
+  return (
+    <Page
+      title={title}
+      breadcrumbs={[{
+        content: 'Voltar',
+        onAction: () => {
+          history.push('/specialtylist');
+        },
+      }]}
+    >
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Layout>
+          <Layout.Section>
+            <Card sectioned>
+              <FormLayout>
+                <Controller
+                  as={(
+                    <TextField
+                      type="text"
+                      label="Nome do procedimento"
+                      error={`${errors.name ? 'Nome obrigatorio' : ''}`}
+                      onChange={() => { }}
+                    />
+                  )}
+                  control={control}
+                  name="name"
+                  rules={{ required: true }}
+                />
+                <Controller
+                  as={(
+                    <TextField
+                      type="text"
+                      label="Codigo"
+                      onChange={() => { }}
+                    />
+                  )}
+                  control={control}
+                  name="code"
+                />
+                <Controller
+                  as={(
+                    <Select
+                      label="Especialidade"
+                      options={specialties}
+                      error={`${errors.specialtyId ? 'Especialidade obrigatorio' : ''}`}
+                      placeholder="Selecione uma especialidade"
+                      onChange={() => { }}
+                    />
+                  )}
+                  control={control}
+                  name="specialtyId"
+                  rules={{ required: true }}
+                />
+                <Controller
+                  as={(
+                    <TextField
+                      type="text"
+                      label="Valor"
+                      onChange={() => { }}
+                    />
+                  )}
+                  control={control}
+                  name="value"
+                />
+              </FormLayout>
+            </Card>
+          </Layout.Section>
+          <Layout.Section>
+            <Stack distribution="equalSpacing">
+              <Button loading={createLoading || updateLoading} onClick={() => { history.push(`/procedureTable/${params.procedureTableId}`); }}>Cancelar</Button>
+              <Button loading={createLoading || updateLoading} submit primary>Salvar</Button>
+            </Stack>
+          </Layout.Section>
+        </Layout>
+      </Form>
+    </Page>
+  );
+};
 
 export default ProcedureForm;
