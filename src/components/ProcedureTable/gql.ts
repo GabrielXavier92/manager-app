e2f6cb16a5784578ffaa153ad85851a77ee27b07
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-07-10 00:08:52 -0300

    move gql from components

---
+++ src/components/ProcedureTable/gql.ts
@@ -0,0 +1,46 @@
+import gql from 'graphql-tag';
+
+export const PROCEDURE_TABLE_FRAGMENT = gql`
+  fragment ProcedureTable on ProcedureTable {
+    id
+    name
+    createdAt
+    updatedAt
+  }
+`;
+
+export const GET_PROCEDURE_TABLES = gql`
+  query GetProcedureTables {
+    getProcedureTables {
+      ...ProcedureTable
+    }
+  }
+  ${PROCEDURE_TABLE_FRAGMENT}
+`;
+
+export const GET_PROCEDURE_TABLE = gql`
+  query GetProcedureTable($id: ID!) {
+    getProcedureTable(id: $id) {
+      ...ProcedureTable
+    }
+  }
+  ${PROCEDURE_TABLE_FRAGMENT}
+`;
+
+export const CREATE_PROCEDURE_TABLE = gql`
+  mutation CreateProcedureTable($input: ProcedureTableInput!) {
+    createProcedureTable(input: $input) {
+      ...ProcedureTable
+    }
+  }
+  ${PROCEDURE_TABLE_FRAGMENT}
+`;
+
+export const UPDATE_PROCEDURE_TABLE = gql`
+  mutation UpdateProcedureTable($id:ID!, $input: ProcedureTableInput!) {
+    updateProcedureTable(id: $id, input: $input) {
+      ...ProcedureTable
+    }
+  }
+  ${PROCEDURE_TABLE_FRAGMENT}
+`;
